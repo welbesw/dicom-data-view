5d2e288be6b4aac585f8d822e104f21125172dd3
William Welbes <williamj@gmail.com>
2015-07-23 13:35:43 -0500

    Added the initial mac app project.

---
+++ DicomDataView/DicomDataView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DicomDataView
+//
+//  Created by William Welbes on 7/23/15.
+//  Copyright (c) 2015 Technomagination, LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
