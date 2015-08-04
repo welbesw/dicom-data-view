ab1ca5a96d0ce1b9f9a315f842fee437dd822225
William Welbes <williamj@gmail.com>
2015-08-04 14:55:46 -0500

    Updated handling of opening document when first loading or no other documents are already open.

--- DicomDataView/DicomDataView/AppDelegate.swift
+++ DicomDataView/DicomDataView/AppDelegate.swift
@@ -11,7 +11,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var isFirstLanuch: Bool = true
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
@@ -21,6 +21,38 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    func applicationDidBecomeActive(notification: NSNotification) {
+        //Handle the app becoming active again
+        println("applicationDidBecomeActive")
+        
+        if(isFirstLanuch) {
+            self.isFirstLanuch = false
+            showOpenDialog()
+            
+        }
+    }
+    
+    func applicationShouldHandleReopen(sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        println("applicationShouldHandleReopen")
+        
+        if (!flag) {    //There are no visible windows - show the open panel
+            showOpenDialog()
+        }
+        
+        return true
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return false
+    }
+    
+    func showOpenDialog() {
+        let documentController = NSDocumentController.sharedDocumentController() as! NSDocumentController
+        if (documentController.documents.count == 0) {
+            //prompt the user to open a document
+            documentController.openDocument(self)
+        }
+    }
 
 }
 
